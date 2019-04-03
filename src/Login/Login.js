699b0ff215bc89d141d20230bd10a048d60058b1
Martin Cantor <andres.cantor-u@mail.escuelaing.edu.co>
2019-04-03 17:20:12 -0500

    Token Login v1

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -8,9 +8,46 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import "./Login.css";
 import logof from './../logof.jpg';
+import axios from "axios";
 
 
 export class Login extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {username: "", password: ""};
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleUserChange = this.handleUserChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        axios.post("https://myuniapp-back.herokuapp.com/accounts/login",
+            {
+                username: this.state.username,
+                password: this.state.password
+            }).then((response) => {
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + response.data.accessToken;
+                localStorage.setItem("accessToken", response.data.accessToken);
+                localStorage.setItem('username', response.data.user.id);
+                localStorage.setItem('password', response.data.user.password);
+            }).catch (function (error) {
+               console.log(error);
+               alert("Wrong credentials");
+        });
+
+    }
+
+    handleUserChange(e){
+        this.setState({username: e.target.value});
+    }
+
+    handlePasswordChange(e){
+        this.setState({password: e.target.value});
+    }
+
+
     render() {
         return (
             <React.Fragment>
@@ -22,7 +59,7 @@ export class Login extends React.Component {
                         <form className="form" onSubmit={this.handleSubmit} name="login-form">
                             <FormControl margin="normal" required fullWidth>
                                 <InputLabel htmlFor="email">Email Address</InputLabel>
-                                <Input id="email" name="email" autoComplete="email" autoFocus />
+                                <Input id="email" name="email" autoComplete="email" autoFocus onChange={this.handleUserChange}/>
                             </FormControl>
                             <FormControl margin="normal" required fullWidth>
                                 <InputLabel htmlFor="password">Password</InputLabel>
@@ -31,6 +68,7 @@ export class Login extends React.Component {
                                     type="password"
                                     id="password"
                                     autoComplete="current-password"
+                                    onChange={this.handlePasswordChange}
                                 />
                             </FormControl>
                             <Button
