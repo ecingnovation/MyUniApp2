af453d3436b3c6d8f8dde7c969873078e80401f8
Daniel Ospina <daniel.ospina-b@mail.escuelaing.edu.co>
2019-04-19 20:15:42 -0500

    Arreglos Varios en la zona de registro, ortografía, iconos, etc.

--- src/Register/RegisterForm.js
+++ src/Register/RegisterForm.js
@@ -6,7 +6,8 @@ import MenuItem from '@material-ui/core/MenuItem';
 import { RegisterTeacher } from "../Register/RegisterTeacher";
 import { RegisterStudent } from "../Register/RegisterStudent";
 import { RegisterAdministrative } from "../Register/RegisterAdministrative";
-
+import { CssBaseline, Paper } from "@material-ui/core";
+import "./Register.css";
 
 export class RegisterForm extends React.Component {
     constructor(props){
@@ -27,26 +28,32 @@ export class RegisterForm extends React.Component {
     
     render() {
         var options= (
-            <React.Fragment>           
-                <FormControl  margin="normal" required fullWidth>
-                    <InputLabel htmlFor="text">Register</InputLabel>
-                    <Select                            
-                        onChange={this.handleRegistryChange}
-                        value={this.state.registry}
-                        inputProps={{
-                            name: 'registry',
-                            id: 'text',
-                        }}
-                        >
-                        <MenuItem value="1">
-                            <em>Select option</em>
-                        </MenuItem>
-                        <MenuItem value={10}>Are you a Teacher? </MenuItem>
-                        <MenuItem value={20}>Are you a Student? </MenuItem>
-                        <MenuItem value={30}>Are you an Administrative? </MenuItem>
-                    </Select>       
-                </FormControl>
-            </React.Fragment>     
+            <React.Fragment>
+                <CssBaseline>
+                <main className="layout">
+                    <Paper className="paper">
+                        <FormControl  margin="normal" required fullWidth>
+                            <InputLabel htmlFor="text">¿Cuál es tu Rol?</InputLabel>
+                            <Select                            
+                                onChange={this.handleRegistryChange}
+                                value={this.state.registry}
+                                inputProps={{
+                                    name: 'registry',
+                                    id: 'text',
+                                }} 
+                                >
+                                <MenuItem value="1">
+                                    <em>Selecciona tu Rol</em>
+                                </MenuItem>
+                                <MenuItem value={10}>Profesor </MenuItem>
+                                <MenuItem value={20}>Estudiante</MenuItem>
+                                <MenuItem value={30}>Administrador</MenuItem>
+                            </Select>       
+                        </FormControl>
+                    </Paper>
+                </main>
+                </CssBaseline>
+            </React.Fragment>
         );
         if (this.state.registry === 10){
             return(  
@@ -80,18 +87,12 @@ export class RegisterForm extends React.Component {
                     </form>              
                 </section> );
 
-        }
-        
-        
+        }   
         return (
             <div>
                {options}
             </div>
         );
-
-
-
     }
-
 }
 
